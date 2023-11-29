20885b33f0acbbe56364048f044e5fd499e7e7c7
Ragamalika Karumuri <karumuri.r@northeastern.edu>
2023-11-28 23:27:24 -0500

    Login.js implement

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -33,4 +33,47 @@ const Login = () => {
         setErrorMessage('An error occurred. Please try again.');
       });
     };
+    return (
+        <div className="container login-container">
+          <div className='row mt-5'>
+            <div className='col-md-4'></div>
+            <div className='col-md-4'>
+              <h2 className="mb-4">Login</h2>
+              <div className="form-group">
+                <label htmlFor="email">Email address:</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="form-group mb-3">
+                <label htmlFor="password">Password:</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <button type="button" className="btn btn-primary" onClick={handleLogin}>
+                Login
+              </button>
+              {errorMessage && (
+                <div className="mt-3 alert alert-danger">{errorMessage}</div>
+              )}
+              {successMessage && (
+                <div className="mt-3 alert alert-success">{successMessage}</div>
+              )}
+            </div>
+          </div>
+        </div>
+      );
 };
+
+export default Login;
