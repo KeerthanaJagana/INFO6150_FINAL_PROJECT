cf2e9ebb5200967ebc86a006c7fc20a2d53c92bb
MeghanaNandaKumar <nandakumar.me@northeastern.edu>
2023-12-03 00:00:36 -0500

    footer

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -505,6 +505,74 @@ export default function Home() {
                 </div>
             </div>      
 
+  <div className="site-footer">
+                <div className="inner first">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-6 col-lg-4">
+                                <div className="widget">
+                                    <h3 className="heading">About Tour</h3>
+                                    <p>
+                                        Far far away, behind the word mountains, far from the
+                                        countries Vokalia and Consonantia, there live the blind texts.
+                                    </p>
+                                </div>
+                                <div className="widget"></div>
+                            </div>
+                            <div className="col-md-6 col-lg-2 pl-lg-5">
+                                <div className="widget">
+                                    <h3 className="heading">Pages</h3>
+                                    <ul className="links list-unstyled">
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">Services</a></li>
+                                        <li><a href="#">About Us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-md-6 col-lg-2">
+                                <div className="widget">
+                                    <h3 className="heading">Trip Types</h3>
+                                    <ul className="links list-unstyled">
+                                        <li><a href="#">One Day</a></li>
+                                        <li><a href="#">Two Day</a></li>
+                                        <li><a href="#">Long Trip</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-md-6 col-lg-4">
+                                <div className="widget">
+                                    <h3 className="heading">Contact</h3>
+                                    <ul className="list-unstyled quick-info links">
+                                        <li className="email"><a href="#">mail@example.com</a></li>
+                                        <li className="phone"><a href="#">+1 222 212 3819</a></li>
+                                        <li className="address">
+                                            <a href="#">43 Raymouth Rd. Baltemoer, London 3910</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="inner dark">
+                    <div className="container">
+                        <div className="row text-center">
+                            <div className="col-md-8 mb-3 mb-md-0 mx-auto">
+                                <p>
+                                    Copyright &copy;
+                                    <script>
+                                        document.write(new Date().getFullYear());
+                                    </script>
+                                    . All Rights Reserved. Travel Blog
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
  )
 
 }
