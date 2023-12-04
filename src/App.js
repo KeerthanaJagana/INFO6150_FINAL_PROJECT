0b451ca3614c8fd79db05316c88c55bdf5c40583
gantab665 <ganta.b@northeastern.edu>
2023-12-04 00:10:01 -0500

    rendering pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home/Home";
 import Login from './Pages/Login/Login';
+import Register from "./Pages/Register/Register";
+import Header from "./Components/Header";
 const AppRoutes = () => {
     return (
       <>
@@ -11,8 +13,12 @@ const AppRoutes = () => {
         <Router>
           <Header></Header>
           <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route path="/login" element={<PublicRoute element={<Login />} />} />
+          <Route path="/register" element={<Register />} />
+         
+           
           </Routes>
         </Router>
       </>
